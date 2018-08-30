602c9085e61f6b41cebbbce5defb210e758aaf77
Alexandra Crisan <36537392+alexa26cris@users.noreply.github.com>
2018-08-30 12:15:43 +0200

    Create markers.js

---
+++ markers.js
@@ -0,0 +1,27 @@
+
+var myLocations = [
+     {
+      title: 'Behind The Mirror', 
+      lat: 52.368123, 
+      lng: 5.187447
+    },
+     {
+      title: 'Salon Bibob', 
+      lat: 52.371084, 
+      lng: 5.179398
+     },
+     {
+      title: 'Haarstudio Marijke', 
+      lat: 52.369891, lng: 5.19101
+     },
+     {
+      title: 'Hairdressers Caesars Beauty', 
+      lat: 52.370118, 
+      lng: 5.179332
+     },
+     {
+      title: 'Hairstyliste Mali', 
+      lat: 52.370228, 
+      lng: 52.370228
+     }
+    ];
